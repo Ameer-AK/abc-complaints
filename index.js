bf595ee8230bf547fd5a0207ae377fc325cdb5bf
AmeerAdhami <aadham01@dcs.bbk.ac.uk>
2021-04-30 04:53:20 +0300

    add session, mongostore, user model, passport auth

--- index.js
+++ index.js
@@ -5,10 +5,17 @@ if (process.env.NODE_ENV !== "production") {
 const express = require('express');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const session = require('express-session');
 const path = require('path');
+const MongoStore = require('connect-mongo');
+const User = require('./models/user');
 const Users = require('./fakedb/users')
 const Complaints = require('./fakedb/complaints')
 
+
+//DB Connection
 const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/abc-complaints';
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
@@ -25,12 +32,53 @@ db.once('open', () => {
 
 app = express();
 
+//Rendering Setup
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
 
+
+//Session Setup
+const secret = process.env.SECRET || 'thisshouldbeabettersecret';
+
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    secret,
+    touchAfter: 24 * 60 * 60
+});
+
+store.on("error", function (e) {
+    console.log("SESSION STORE ERROR", e);
+})
+
+//TODO this
+const sessionConfig = {
+    store,
+    name: 'xzr',
+    secret,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        // secure: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7 * 1,
+        maxAge: 1000 * 60 * 60 * 24 * 7 * 1
+    }
+}
+app.use(session(sessionConfig));
+
+//Passport Auth Setup
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+//Routes
 app.get('/', (req, res) => {
     res.render('home');
     console.log(mongoose.connection.readyState);
